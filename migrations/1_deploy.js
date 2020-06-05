9e2afa766a443cf7ca56283ed4401f827cdc3870
Brendan Chou <brendanchou@msn.com>
2020-06-04 19:43:09 -0700

    add tests and coverage (#39)

---
+++ migrations/1_deploy.js
@@ -0,0 +1,38 @@
+/*
+
+    Copyright 2020 dYdX Trading Inc.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+*/
+
+// ============ Contracts ============
+
+const OasisV1SimpleExchangeWrapper = artifacts.require('OasisV1SimpleExchangeWrapper');
+const OasisV2SimpleExchangeWrapper = artifacts.require('OasisV2SimpleExchangeWrapper');
+const OasisV3SimpleExchangeWrapper = artifacts.require('OasisV3SimpleExchangeWrapper');
+const OasisV1MatchingExchangeWrapper = artifacts.require('OasisV1MatchingExchangeWrapper');
+const OasisV3MatchingExchangeWrapper = artifacts.require('OasisV3MatchingExchangeWrapper');
+const OpenDirectlyExchangeWrapper = artifacts.require('OpenDirectlyExchangeWrapper');
+const SaiDaiExchangeWrapper = artifacts.require('SaiDaiExchangeWrapper');
+const ZeroExV2ExchangeWrapper = artifacts.require('ZeroExV2ExchangeWrapper');
+const ZeroExV2MultiOrderExchangeWrapper = artifacts.require('ZeroExV2MultiOrderExchangeWrapper');
+const TestExchangeWrapper = artifacts.require('TestExchangeWrapper');
+
+// ============ Main Migration ============
+
+const migration = async (deployer, network, accounts) => {
+  await deployer.deploy(TestExchangeWrapper);
+};
+
+module.exports = migration;
---
+++ test/helpers/Expect.ts
@@ -0,0 +1,57 @@
+import chai from 'chai';
+import BigNumber from 'bignumber.js';
+chai.use(require('chai-bignumber')(BigNumber));
+
+import { address, BigNumberable } from '../../src/types';
+
+const REQUIRE_MSG = 'Returned error: VM Exception while processing transaction: revert';
+const ASSERT_MSG = 'Returned error: VM Exception while processing transaction: invalid opcode';
+
+// For solidity function calls that violate require()
+export async function expectThrow(promise: Promise<any>, reason?: string) {
+  try {
+    await promise;
+    throw new Error('Did not throw');
+  } catch (e) {
+    assertCertainError(e, REQUIRE_MSG);
+    if (reason && process.env.COVERAGE !== 'true') {
+      assertCertainError(e, `${REQUIRE_MSG} ${reason}`);
+    }
+  }
+}
+
+// For solidity function calls that violate assert()
+export async function expectAssertFailure(promise: Promise<any>) {
+  try {
+    await promise;
+    throw new Error('Did not throw');
+  } catch (e) {
+    assertCertainError(e, ASSERT_MSG);
+  }
+}
+
+// Helper function
+function assertCertainError(error: Error, expected_error_msg?: string) {
+  // This complication is so that the actual error will appear in truffle test output
+  const message = error.message;
+  const matchedIndex = message.search(expected_error_msg);
+  let matchedString = message;
+  if (matchedIndex === 0) {
+    matchedString = message.substring(matchedIndex, matchedIndex + expected_error_msg.length);
+  }
+  chai.expect(matchedString).to.eq(expected_error_msg);
+}
+
+export function expect(item: any, message?: string): Chai.Assertion {
+  return chai.expect(item, message);
+}
+
+export function expectAddressesEqual(arg1: address, arg2: address, message?: string) {
+  chai.expect(typeof arg1).to.equal('string', 'expectAddressesEqual arg1 type');
+  chai.expect(typeof arg2).to.equal('string', 'expectAddressesEqual arg2 type');
+  expect(arg1.toLowerCase()).to.equal(arg2.toLowerCase(), message);
+}
+
+export function expectBN(expectedBN: BigNumberable, message?: string): Chai.Assertion {
+  return (chai.expect(new BigNumber(expectedBN), message) as any).to.be.bignumber;
+}
